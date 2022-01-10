319ffc228a4f5ab526aab371d63e1eee99877b66
Kristina Jovanovska <kristinajovanovska993@gmail.com>
2022-01-10 15:54:21 +0100

    sql queries ---> ?

--- lotr_bookstore.py
+++ lotr_bookstore.py
@@ -1,4 +1,4 @@
-import mysql.connector
+
 import tkinter as tk
 from tkinter import ttk
 from calendartime import DTP
@@ -14,7 +14,7 @@ class MyApp(tk.Tk):
 
         self.title('LOTR bookstore')
         self.iconbitmap('')
-        self.geometry('800x600')
+        self.geometry('1400x600')
         self.rowconfigure(0, weight=1)
         self.columnconfigure(0, weight=1)
         self.resizable(False, False)
@@ -65,6 +65,18 @@ class FrameOne(tk.Frame):
         self.enter_book_name = tk.Entry(self, width=20, borderwidth=5)
         self.enter_book_name.grid(row=1, column=2, padx=50)
 
+        # rent_date_label = tk.Label(self, text='Rent Date', font='bold, 15', bg='#FFAB49')
+        # rent_date_label.grid(row=0, column=3, pady=10)
+        #
+        # self.enter_rent_date = tk.Entry(self, width=20, borderwidth=5)
+        # self.enter_rent_date.grid(row=1, column=3, padx=50)
+
+        # return_date_label = tk.Label(self, text='Return Date', font='bold, 15', bg='#FFAB49')
+        # return_date_label.grid(row=0, column=4, pady=10)
+        #
+        # self.enter_return_date = tk.Entry(self, width=20, borderwidth=5)
+        # self.enter_return_date.grid(row=1, column=4, padx=50)
+
         # buttons
 
         button_enter = tk.Button(self, text='ENTER', font='bold, 18', command=lambda: self.insert_values(),
@@ -78,7 +90,6 @@ class FrameOne(tk.Frame):
         button_delete = tk.Button(self, text='DELETE', font='bold, 18', command=lambda: self.delete_values(),
                                   bg='#FBDB48', borderwidth=8, relief="raised")
         button_delete.grid(row=2, column=3, padx=60, pady=40)
-
         # date time picker
 
         self.enter_rent_date_btn = tk.Button(self, text='Rent Date', font='bold, 12', bg='#FBDB48',
@@ -87,11 +98,23 @@ class FrameOne(tk.Frame):
         self.enter_rent_date_btn.grid(row=1, column=3, padx=50)
 
         self.enter_return_date_btn = tk.Button(self, text='Return Date', font='bold, 12', bg='#FBDB48',
-                                             command=lambda: self.get_return_date(),
-                                             borderwidth=8, relief="raised")
+                                               command=lambda: self.get_return_date(),
+                                               borderwidth=8, relief="raised")
         self.enter_return_date_btn.grid(row=1, column=4, padx=50)
 
+        # rent_date_label = tk.Label(self, text=f'{}', font='bold, 15', bg='#FFAB49')
+        # rent_date_label.grid(row=0, column=3, pady=10)
+
         # database
+
+        self.my_cursor = self.mydb.cursor(buffered=True)
+        self.my_cursor2 = self.mydb.cursor(buffered=True)
+        self.book_name = ' '
+        self.return_date = ' '
+
+        # log = tk.Toplevel(self)
+        # log.transient(self)
+        # log.title('Users')
 
         # setup treeview
         columns = ('First Name', 'Last Name', 'Book', 'Rent Date', 'Return Date')
@@ -111,10 +134,16 @@ class FrameOne(tk.Frame):
                                'UNION SELECT * FROM users RIGHT JOIN books ON users.book_name=books.book_name '
                                'GROUP BY users.book_name, books.book_name')
 
+
         for r in tuple(set(self.my_cursor.fetchall())):
             r1 = list(r)
             del r1[2]
             self.tree.insert('', 'end', value=tuple(r1))
+
+        # # scrollbar
+        # sb = tk.Scrollbar(self, orient=tk.VERTICAL, command=tree.yview)
+        # sb.grid(row=3, column=3, sticky='ns')
+        # tree.config(yscrollcommand=sb.set)
 
     def rent_date(self):
 
@@ -162,54 +191,62 @@ class FrameOne(tk.Frame):
         self.return1_date()
         print(self.return1)
         print(self.rent1)
-        self.my_cursor.execute(f"SELECT * FROM books WHERE book_name='{self.enter_book_name.get()}'")
+        command1 = "SELECT * FROM books WHERE book_name = ? "
+        self.my_cursor.execute(command1, str(self.enter_book_name.get()))
 
         try:
             # the book is already in the db table
 
             self.my_cursor.execute("SET foreign_key_checks=0")
 
-            self.my_cursor.execute(f"UPDATE books SET rent_datetime='{self.rent1}', "
-                                   f"return_datetime='{self.return1}' "
-                                   f"WHERE book_name='{self.book_name}' ")
+            command2 = "UPDATE books SET rent_datetime=?, return_datetime=?" \
+                       "WHERE book_name=? "
+            self.my_cursor.execute(command2, str(self.rent1), str(self.return1), str(self.book_name))
 
-            self.my_cursor.execute(f"UPDATE users SET first_name='{self.enter_first_name.get()}', "
-                                   f"last_name='{self.enter_last_name.get()}', "
-                                   f"WHERE book_name='{self.my_cursor.fetchone()[0]}'")
+            command3 = "UPDATE users SET first_name=?, last_name=?" \
+                       "WHERE book_name=? "
+            self.my_cursor.execute(command3, str(self.enter_first_name.get()), str(self.enter_last_name.get()),
+                                   str(self.my_cursor.fetchone()[0]))
 
             self.my_cursor.execute("SET foreign_key_checks=1")
 
         except TypeError:
             # the book is not in the db table
-            self.my_cursor.execute(f"INSERT INTO books VALUES('{self.enter_book_name.get()}',"
-                                   f"'{self.rent1}', '{self.return1}') ")
-            self.my_cursor.execute(f"INSERT INTO users VALUES('{self.enter_first_name.get()}',"
-                                   f"'{self.enter_last_name.get()}', '{self.enter_book_name.get()}')")
+            command4 = "INSERT INTO books VALUES(?, ?, ?) "
+            self.my_cursor.execute(command4, str(self.enter_book_name.get()), str(self.rent1), str(self.return1))
+
+            command5 = "INSERT INTO users VALUES(?, ?, ?)"
+            self.my_cursor.execute(command5, str(self.enter_first_name.get()), str(self.enter_last_name.get()),
+                                   str(self.enter_book_name.get()))
 
         self.mydb.commit()
         self.update_table()
 
     def delete_values(self):
         self.my_cursor.execute("SET foreign_key_checks=0")
-        self.my_cursor.execute(f"SELECT book_name FROM users "
-                               f"WHERE first_name='{self.enter_first_name.get()}'")
+        command6 = "SELECT book_name FROM users WHERE first_name=? "
+        self.my_cursor.execute(command6, str(self.enter_first_name.get()))
         self.book_name = self.my_cursor.fetchone()[0]
         # print(self.book_name[0])
 
-        self.my_cursor.execute(f"DELETE FROM books WHERE book_name='{self.book_name}'")
-        self.my_cursor.execute(f"DELETE FROM users WHERE first_name='{self.enter_first_name.get()}'")
+        command7 = "DELETE FROM books WHERE book_name= ? "
+        self.my_cursor.execute(command7, str(self.book_name))
+
+        command8 = "DELETE FROM users WHERE first_name= ?"
+        self.my_cursor.execute(command8, str(self.enter_first_name.get()))
 
         self.mydb.commit()
         self.update_table()
 
     def update_return_date(self):
         self.return1_date()
-        self.my_cursor.execute(f"SELECT * FROM books WHERE book_name='{self.enter_book_name.get()}'")
+        command9 = "SELECT * FROM books WHERE book_name= ?"
+        self.my_cursor.execute(command9, str(self.enter_book_name.get()))
         self.book_name = self.my_cursor.fetchone()[0]
         # print(self.book_name)
 
-        self.my_cursor.execute(f"UPDATE books SET return_datetime='{self.return1}' "
-                               f"WHERE book_name='{self.book_name}'")
+        command10 = "UPDATE books SET return_datetime=? WHERE book_name=? "
+        self.my_cursor.execute(command10, str(self.return1), str(self.book_name))
 
         self.mydb.commit()
         self.update_table()
